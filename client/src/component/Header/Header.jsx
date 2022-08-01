7e9eb6ba26f7dd03809e9006c2000c2ea0999933
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-08-01 11:38:23 -0700

    styling header and logout fuctionality

--- client/src/component/Header/Header.jsx
+++ client/src/component/Header/Header.jsx
@@ -1,14 +1,34 @@
 import './Header.css';
 
 function Header() {
+    let token = JSON.parse(localStorage.getItem("userData"));
+    if (token) {
+        token = token.token;
+    }
+    console.log(token);
+
+    // logout function
+    const logout = () => {
+        localStorage.setItem("userData", JSON.stringify({}));
+        window.location.reload(false);
+    }
+
     return (
         <div id="header">
             <div id='titleDiv'>
                 <a id="title" href="/">Budget App</a>
             </div>
             <div id='nav'>
-                <a href="/login">Login</a>
-                <a href="/signup">Signup</a>
+                {
+                    token ? (
+                        <p onClick={logout}>Logout</p>
+                    ) : (
+                        <>
+                            <a href="/login">Login</a>
+                            <a href="/signup">Signup</a>
+                        </>
+                    )
+                }
             </div>
         </div>
     );
