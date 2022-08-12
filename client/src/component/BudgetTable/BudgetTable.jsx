32ce151848586145f899760b807cd8f8ee977ae2
Keian Anthony <KeianAnthony1@gmail.com>
2022-08-12 11:55:34 -0700

    tons of bug fixes, added some todos so we have more direction next week

--- client/src/component/BudgetTable/BudgetTable.jsx
+++ client/src/component/BudgetTable/BudgetTable.jsx
@@ -1,3 +1,5 @@
+import './BudgetTable.css';
+
 function BudgetTable({ budget }) {
     return (
         <div id="budgetDisplay">
@@ -20,14 +22,13 @@ function BudgetTable({ budget }) {
                 <h2>Additional Monthly Expenses: {budget.additionalExpenses}</h2>
             </div>
             <div className="flexChildren">
-                {console.log(budget)}
-                <h2>Money Remaining for Monthly Use: $ {budget.moneyRemainingMonthly}</h2>
+                <h2>Money Remaining for Monthly Use: ${budget.moneyRemainingMonthly}</h2>
             </div>
             <div className="flexChildren">
-                <h2>Money Remaining for Daily Use: $ {budget.moneyRemainingDaily}</h2>
+                <h2>Money Remaining for Daily Use: ${budget.moneyRemainingDaily}</h2>
             </div>
         </div>
-    )
-};
+    );
+}
 
 export default BudgetTable;
---
+++ client/src/component/Modal/Modal.jsx
@@ -0,0 +1,79 @@
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+
+function Modal({ open, handleClose, handleInput, handleSubmit }) {
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Create a budget</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    Fill out the below questions to create a customized budget!
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Budget name"
+                    fullWidth
+                    variant="standard"
+                    onChange={handleInput}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="income"
+                    label="Monthly income"
+                    fullWidth
+                    variant="standard"
+                    type="number"
+                    onChange={handleInput}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="expenses"
+                    label="Monthly expenses"
+                    fullWidth
+                    variant="standard"
+                    type="number"
+                    onChange={handleInput}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="additional-expenses"
+                    label="Additional monthly expenses"
+                    fullWidth
+                    variant="standard"
+                    type="number"
+                    onChange={handleInput}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="retirement-savings"
+                    label="% of income to save for retirement"
+                    fullWidth
+                    variant="standard"
+                    type="number"
+                    onChange={handleInput}
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="personal-savings"
+                    label="$ amount of desired monthly savings"
+                    fullWidth
+                    variant="standard"
+                    type="number"
+                    onChange={handleInput}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Close</Button>
+                <Button onClick={handleSubmit}>Submit</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default Modal;
